51cfcc615257912ba9cd10c4a72f66fcc583e4ef
ChengyueWang <44804690+ChengyueWang@users.noreply.github.com>
2025-05-08 00:29:37 -0700

    Update loadB_from_dram.cpp

--- RSN-XNN-FPGA/design/pl_src/loadB_from_dram.cpp
+++ RSN-XNN-FPGA/design/pl_src/loadB_from_dram.cpp
@@ -4,14 +4,7 @@ void loadB_from_dram(hls::stream<uop_loadB_dram_type>& stream_uOP_loadB_dram,
                      ap_uint<512>* inB_port0, ap_uint<512>* inB_port1,
                      hls::stream<ap_uint<1024>>& stream_from_dramB_to_memCoreB0,
                      hls::stream<ap_uint<1024>>& stream_from_dramB_to_memCoreB1) {
-#ifdef SW_EMU_PRINT
-  std::ofstream outFile(
-      "/home/cw4/github/versal-float32/20-inputlen384/design/pl_src/output/loadB_from_dram.txt",
-      std::ios_base::app);
-  if (!outFile.is_open()) {
-    std::cerr << "Unable to open file for writing." << std::endl;
-  }
-#endif
+
   uop_loadB_dram_type uOP;
   bool is_last_uOP = false;
 WHILE_LOOP:
@@ -19,13 +12,6 @@ WHILE_LOOP:
     uOP = stream_uOP_loadB_dram.read();
     is_last_uOP = uOP.is_last_uOP;
 
-#ifdef SW_EMU_PRINT
-    outFile << "uOP.is_loading_bias = " << uOP.is_loading_bias
-            << " uOP.start_address = " << uOP.start_address
-            << " uOP.chunck_size = " << uOP.chunck_size
-            << " uOP.load_to_memcoreB0 = " << uOP.load_to_memcoreB0
-            << " uOP.load_to_memcoreB1 = " << uOP.load_to_memcoreB1 << std::endl;
-#endif
 
     if (uOP.is_loading_bias == true) {
     LOOP_BIAS:
@@ -38,14 +24,7 @@ WHILE_LOOP:
         } else {
           stream_from_dramB_to_memCoreB1.write(temp);
         }
-#ifdef SW_EMU_PRINT
-        for (int word = 0; word < 32; word++) {  // outFile
-          UNION_FP_UINT32 temp_union;
-          temp_union.uint32_val = temp(word * 32 + 31, word * 32);  // outFile
-          outFile << temp_union.float_val << " ";
-        }
-        outFile << std::endl;
-#endif
+
       }
     } else {
       for (uint32_t e = 0; e < uOP.chunck_size / 16; e = e + 128) {
@@ -60,20 +39,10 @@ WHILE_LOOP:
           } else if (uOP.load_to_memcoreB1 == true) {
             stream_from_dramB_to_memCoreB1.write(temp);
           }
-#ifdef SW_EMU_PRINT
-          for (int word = 0; word < 32; word++) {  // outFile
-            UNION_FP_UINT32 temp_union;
-            temp_union.uint32_val = temp(word * 32 + 31, word * 32);  // outFile
-            outFile << temp_union.float_val << " ";
-          }
-          outFile << std::endl;
-#endif
+
         }
       }
     }
   }
-#ifdef SW_EMU_PRINT
-  outFile << "EXIT loadB_from_dram   ==========================   ";
-  outFile.close();
-#endif
+
 };
