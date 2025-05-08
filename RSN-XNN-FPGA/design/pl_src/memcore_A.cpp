7cdf7d19239b832641f24a58f85d3468a99fbbab
ChengyueWang <44804690+ChengyueWang@users.noreply.github.com>
2025-05-08 00:31:03 -0700

    Update memcore_A.cpp

--- RSN-XNN-FPGA/design/pl_src/memcore_A.cpp
+++ RSN-XNN-FPGA/design/pl_src/memcore_A.cpp
@@ -8,31 +8,12 @@ ap_uint<64> concat_32b_to_64b_coreA(ap_uint<32> in0, ap_uint<32> in1) {
 }
 
 void loadA_from_dramStream(
-#ifdef SW_EMU_PRINT
-    int core_id,
-#endif
+
     ap_uint<32> buf_A[BUFA_DIM1][BUFA_DIM2], bool enable_load_from_dram, ap_uint<9> size_dim1,
     ap_uint<8> size_dim2, hls::stream<ap_uint<1024>> &stream_from_dramA) {
 
-#ifdef SW_EMU_PRINT
-  std::ofstream outFile(
-      "/home/cw4/github/versal-float32/20-inputlen384/design/pl_src/output/memcoreA" +
-          std::to_string(core_id) + "_loadA_from_dramStream.txt",
-      std::ios_base::app);
-  if (!outFile.is_open()) {
-    std::cerr << "Unable to open file for writing." << std::endl;
-  }
-  outFile << "ENTER loadA_from_dramStream ==========================   ";
-#endif
+
   if (enable_load_from_dram) {
-#ifdef SW_EMU_PRINT
-    // initialize the buffer
-    for (uint16_t dim1 = 0; dim1 < BUFA_DIM1; dim1++) {
-      for (uint16_t dim2 = 0; dim2 < BUFA_DIM2; dim2++) {
-        buf_A[dim1][dim2] = 0;
-      }
-    }
-#endif
 
     // block size 128x64
     for (uint16_t dim1 = 0; dim1 < size_dim1 / 128; dim1++) {
@@ -42,14 +23,7 @@ void loadA_from_dramStream(
 // each iteration get 16 32-bit data from dram at row0, and 16 32-bit from row1
 #pragma HLS PIPELINE II = 1
             ap_uint<1024> data_1024b = stream_from_dramA.read();
-#ifdef SW_EMU_PRINT
-            for (int word = 0; word < 16; word++) {  // outFile
-              UNION_FP_UINT32 temp_union;
-              temp_union.uint32_val = data_1024b(word * 32 + 31, word * 32);  // outFile
-              outFile << temp_union.float_val << " ";
-            }
-            outFile << std::endl;
-#endif
+
             for (uint16_t i = 0; i < 16; i++) {
               buf_A[dim1 * 128 + block_r * 2][dim2 * 64 + block_c * 16 + i] =
                   data_1024b(32 * i + 31, 32 * i);
@@ -61,39 +35,14 @@ void loadA_from_dramStream(
       }
     }
 
-#ifdef SW_EMU_PRINT
-    // Print matrix size
-    outFile << "Size: (" << BUFA_DIM1 << ", " << BUFA_DIM2 << ")\n";
-    // Print column headers
-    outFile << "   ";  // Indentation for row headers
-    for (int j = 0; j < BUFA_DIM2; j++) {
-      outFile << std::setw(3) << j;
-    }
-    outFile << "\n";
 
-    // Print matrix data with row headers
-    for (int i = 0; i < BUFA_DIM1; i++) {
-      outFile << std::setw(3) << i;  // Row header
-      for (int j = 0; j < BUFA_DIM2; j++) {
-        UNION_FP_UINT32 temp_union;
-        temp_union.uint32_val = buf_A[i][j];
-        outFile << std::setw(3) << std::fixed << std::setprecision(0) << temp_union.float_val;
-      }
-      outFile << "\n";
-    }
-#endif
   }
 
-#ifdef SW_EMU_PRINT
-  outFile << "EXIT loadA_from_dramStream   ==========================   ";
-  outFile.close();
-#endif
+
 }
 
 void sendA_to_aie(
-#ifdef SW_EMU_PRINT
-    int core_id,
-#endif
+
     ap_uint<32> buf_A[BUFA_DIM1][BUFA_DIM2], bool enable_sendrecv_aie, uint16_t size_dim1,
     uint16_t size_dim2, ap_uint<3> compute_tile_access_A, ap_uint<3> compute_tile_access_B,
     hls::stream<ap_uint<64>> &to_computeCore0_A0_CASC0,
@@ -112,15 +61,6 @@ void sendA_to_aie(
     hls::stream<ap_uint<64>> &to_computeCore0_A3_CASC1,
     hls::stream<ap_uint<64>> &to_computeCore0_A3_CASC2,
     hls::stream<ap_uint<64>> &to_computeCore0_A3_CASC3) {
-#ifdef SW_EMU_PRINT
-  std::ofstream outFile(
-      "/home/cw4/github/versal-float32/20-inputlen384/design/pl_src/output/memcoreA" +
-          std::to_string(core_id) + "_sendA_to_aie.txt",
-      std::ios_base::app);
-  if (!outFile.is_open()) {
-    std::cerr << "Unable to open file for writing." << std::endl;
-  }
-#endif
 
   if (enable_sendrecv_aie) {
     for (uint16_t reuse_a = 0; reuse_a < compute_tile_access_A * 128; reuse_a += 128) {
@@ -141,17 +81,7 @@ void sendA_to_aie(
 #pragma HLS bind_op variable = row_addr op = add impl = fabric
 #pragma HLS bind_op variable = col_addr op = add impl = fabric
 
-#ifdef SW_EMU_PRINT
-                outFile << "row_addr " << row_addr << " col_addr " << col_addr << "  ";
-                for (int i = 0; i < 4; i++) {
-                  for (int j = 0; j < 8; j++) {
-                    UNION_FP_UINT32 temp_union;
-                    temp_union.uint32_val = buf_A[row_addr + i][col_addr + j];
-                    outFile << temp_union.float_val << " ";
-                  }
-                }
-                outFile << std::endl;
-#endif
+
 
 #pragma HLS PIPELINE II = 1
                 ap_uint<64> data_A0_casc0, data_A0_casc1, data_A0_casc2, data_A0_casc3;
@@ -215,16 +145,10 @@ void sendA_to_aie(
       }
     }
   }
-#ifdef SW_EMU_PRINT
-  outFile << "EXIT sendA_to_aie   ==========================   ";
-  outFile.close();
-#endif
+
 }
 
 void memcore_A(
-#ifdef SW_EMU_PRINT
-    int core_id,
-#endif
 
     hls::stream<uop_memcore_A_type> &stream_uOP_memcore_A,
     hls::stream<ap_uint<1024>> &stream_from_dramA,
@@ -282,9 +206,7 @@ WHILE_LOOP:
     if (is_computing_buf1 == 1) {
       // first send (write) then read; otherwise simulation will be blocked
       sendA_to_aie(
-#ifdef SW_EMU_PRINT
-          core_id,
-#endif
+
           buf1_A, enable_send_to_aie, one_compute_tile_dim1, one_compute_tile_dim2,
           compute_tile_access_A, compute_tile_access_B, to_computeCore0_A0_CASC0,
           to_computeCore0_A0_CASC1, to_computeCore0_A0_CASC2, to_computeCore0_A0_CASC3,
@@ -293,15 +215,11 @@ WHILE_LOOP:
           to_computeCore0_A2_CASC2, to_computeCore0_A2_CASC3, to_computeCore0_A3_CASC0,
           to_computeCore0_A3_CASC1, to_computeCore0_A3_CASC2, to_computeCore0_A3_CASC3);
       loadA_from_dramStream(
-#ifdef SW_EMU_PRINT
-          core_id,
-#endif
+
           buf0_A, enable_load_from_dram, one_mem_tile_dim1, one_mem_tile_dim2, stream_from_dramA);
     } else {
       sendA_to_aie(
-#ifdef SW_EMU_PRINT
-          core_id,
-#endif
+
           buf0_A, enable_send_to_aie, one_compute_tile_dim1, one_compute_tile_dim2,
           compute_tile_access_A, compute_tile_access_B, to_computeCore0_A0_CASC0,
           to_computeCore0_A0_CASC1, to_computeCore0_A0_CASC2, to_computeCore0_A0_CASC3,
@@ -310,9 +228,7 @@ WHILE_LOOP:
           to_computeCore0_A2_CASC2, to_computeCore0_A2_CASC3, to_computeCore0_A3_CASC0,
           to_computeCore0_A3_CASC1, to_computeCore0_A3_CASC2, to_computeCore0_A3_CASC3);
       loadA_from_dramStream(
-#ifdef SW_EMU_PRINT
-          core_id,
-#endif
+
           buf1_A, enable_load_from_dram, one_mem_tile_dim1, one_mem_tile_dim2, stream_from_dramA);
     }
 
